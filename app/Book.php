03b15960673dc8fae1ea6ba6d01cb5b35fe47b5f
supyae <supyaethandar@yahoo.com>
2015-09-07 10:47:40 +0630

    change book model

--- app/Book.php
+++ app/Book.php
@@ -3,23 +3,22 @@
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model {
+   <?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model {
     public $table = 'books';
     protected $primaryKey = 'id';
     public  $timestamps = true;
-    protected $fillable = ['title','description','author_id','publishing_house','doj'];
-
-
+    protected $fillable = ['title','description','authorId','genreId','pubHouse','doj'];
     // model relationship with author;
     public function author(){
         return $this->belongsTo('App\Author','authorId');
     }
-
-
     //model relationship with genre;
     public function genre(){
-        return $this->hasOne('App\Genre','id');
+        return $this->belongsTo('App\Genre','genreId');
     }
-
-
 
 }
